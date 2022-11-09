7fe36db9a76c740a781b2a4fb171576f7608817c
Daiki Sakuragi <84183610+DaikiSakuragi@users.noreply.github.com>
2022-11-09 08:59:54 -0300

    Crud Computadores

--- MvcLabManager/Controllers/ComputerController.cs
+++ MvcLabManager/Controllers/ComputerController.cs
@@ -28,4 +28,51 @@ public class ComputerController : Controller
         }
         return View(computer);
     }
+
+    public IActionResult Create(){
+                
+        return View();
+    }
+
+    public IActionResult Creation(int id, string ram, string processor){
+        if(_context.Computers.Find(id) == null)
+        {
+            _context.Computers.Add(new Computer(id,ram,processor));
+            _context.SaveChanges();
+            return RedirectToAction("Create");
+        }
+        else
+        {
+           return Content("Já existe um computador com esse id");
+        }
+       
+    }
+
+    public IActionResult Delete(int id){
+        _context.Computers.Remove(_context.Computers.Find(id));
+        _context.SaveChanges();
+        return View();
+    }
+
+    public IActionResult Update(int id){
+        Computer computer = _context.Computers.Find(id);
+        if(computer == null)
+        {
+            return Content("Esse computador não existe!");
+        }
+        else
+        {
+           return View(computer);
+        }
+    }
+
+    public IActionResult Updation(int id, string ram, string processor){
+        Computer computer = _context.Computers.Find(id);
+
+        computer.Ram = ram;
+        computer.Processor = processor;
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    
 }
